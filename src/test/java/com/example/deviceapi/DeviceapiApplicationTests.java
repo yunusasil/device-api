c29f9bdbaa3024c632da3621a8f9152ae88b2acd
yunusasil <yemreasil@gmail.com>
2025-06-01 18:39:55 +0200

    Initial commit

---
+++ src/test/java/com/example/deviceapi/DeviceapiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.deviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DeviceapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
